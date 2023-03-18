caca524a1be2d87a69862b0eb141d693de46a304
Kincent Lan <lan.k@northeastern.edu>
2023-03-18 18:42:20 -0400

    Deploy React app to GitHub Pages

---
+++ precache-manifest.3dd7549b0c2495858681b0f4c4d45798.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "5df5fd9d63376278645c",
+    "url": "/AriannaBday/static/css/main.78b5abc2.chunk.css"
+  },
+  {
+    "revision": "5df5fd9d63376278645c",
+    "url": "/AriannaBday/static/js/main.5df5fd9d.chunk.js"
+  },
+  {
+    "revision": "92bf2f06d2a0d81614c3",
+    "url": "/AriannaBday/static/js/1.92bf2f06.chunk.js"
+  },
+  {
+    "revision": "29d173710a62fb4b4840",
+    "url": "/AriannaBday/static/css/2.00e709f0.chunk.css"
+  },
+  {
+    "revision": "29d173710a62fb4b4840",
+    "url": "/AriannaBday/static/js/2.29d17371.chunk.js"
+  },
+  {
+    "revision": "2081983fdd93f02b2493",
+    "url": "/AriannaBday/static/js/runtime~main.2081983f.js"
+  },
+  {
+    "revision": "6d89dd74c52b0343268fd8a39f357b4a",
+    "url": "/AriannaBday/static/media/1.6d89dd74.jpg"
+  },
+  {
+    "revision": "ea08a073b527d3a8a4f1edee2a93c6e1",
+    "url": "/AriannaBday/static/media/2.ea08a073.jpg"
+  },
+  {
+    "revision": "597db7e19c9115a47aa9ebeeaa5bfa55",
+    "url": "/AriannaBday/static/media/3.597db7e1.jpg"
+  },
+  {
+    "revision": "7c6d8f8e3cfff5d3892d0de076d5c83c",
+    "url": "/AriannaBday/static/media/4.7c6d8f8e.jpg"
+  },
+  {
+    "revision": "734de95c82dcbea61576206d2a8f8e00",
+    "url": "/AriannaBday/static/media/5.734de95c.jpg"
+  },
+  {
+    "revision": "94b856ec35e56f897c0769334ca407ff",
+    "url": "/AriannaBday/static/media/6.94b856ec.jpg"
+  },
+  {
+    "revision": "b65e04eea3b1e88f7ce9b1df87803ef3",
+    "url": "/AriannaBday/static/media/family-jie-jie.b65e04ee.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/AriannaBday/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/AriannaBday/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/AriannaBday/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/AriannaBday/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "102b2fa29756c4748cd0381512e6184c",
+    "url": "/AriannaBday/index.html"
+  }
+];
